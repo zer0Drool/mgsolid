8a5f92e81ecbc8c516d03e5cc71c56e858a30418
tobisey <tobi.seymour@gmail.com>
2021-03-22 07:36:00 +0000

    tween

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 let scene, camera, renderer, rig, raycaster, mouse, container;
 
-let ballPositions = [
+let balls = [
     {
         x: 0,
         y: 0,
@@ -87,7 +87,7 @@ let ballPositions = [
     }
 ];
 
-let cylinderPositions = [
+let connectors = [
     {
         x: 0,
         y: 0,
@@ -210,6 +210,107 @@ let cylinderPositions = [
     },
 ];
 
+legs = [
+    {
+        x: 0,
+        y: -1,
+        z: 2,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: 0,
+        y: -1,
+        z: -2,
+        r: 0.3,
+        l: 2
+    },
+    // {
+    //     x: 0,
+    //     y: -1.5,
+    //     z: 6,
+    //     r: 0.3,
+    //     l: 3
+    // },
+    // {
+    //     x: 0,
+    //     y: -1.5,
+    //     z: -6,
+    //     r: 0.3,
+    //     l: 3
+    // },
+    {
+        x: 0,
+        y: -1,
+        z: 10,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: 0,
+        y: -1,
+        z: -10,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: 3,
+        y: -1,
+        z: 8,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: 3,
+        y: -1,
+        z: -8,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: -3,
+        y: -1,
+        z: 8,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: -3,
+        y: -1,
+        z: -8,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: 3,
+        y: -1,
+        z: 4,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: 3,
+        y: -1,
+        z: -4,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: -3,
+        y: -1,
+        z: 4,
+        r: 0.3,
+        l: 2
+    },
+    {
+        x: -3,
+        y: -1,
+        z: -4,
+        r: 0.3,
+        l: 2
+    },
+];
+
 function init() {
 
     scene = new THREE.Scene();
@@ -222,7 +323,7 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    camera.position.z = 25;
+    camera.position.z = 20;
 
     raycaster = new THREE.Raycaster();
     mouse = new THREE.Vector2();
@@ -233,24 +334,30 @@ function init() {
     scene.add(container);
 
     container.position.set(0, 0, 0);
+    container.rotation.x = Math.PI / 8;
+    container.rotation.y = Math.PI / -7;
 
     var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLight.position.set(20, 20, 20);
     scene.add( directionalLight );
 
-    for (var i = 0; i < ballPositions.length; i++) {
+    for (var i = 0; i < balls.length; i++) {
         makeBalls(i);
     };
 
-    for (var i = 0; i < cylinderPositions.length; i++) {
-        makeCylinders(i);
+    for (var i = 0; i < connectors.length; i++) {
+        makeConnectors(i);
+    };
+
+    for (var i = 0; i < legs.length; i++) {
+        makeLegs(i);
     };
 
 };
 
 function makeBalls(i) {
 
-    const geometry = new THREE.SphereGeometry(ballPositions[i].r, 32, 32);
+    const geometry = new THREE.SphereGeometry(balls[i].r, 32, 32);
     const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
     sphere = new THREE.Mesh( geometry, material );
     container.add(sphere);
@@ -258,13 +365,15 @@ function makeBalls(i) {
     sphere.highlight = true;
     // console.log(sphere.highlight);
 
-    sphere.position.set(ballPositions[i].x, ballPositions[i].y, ballPositions[i].z);
+    sphere.position.set(balls[i].x, balls[i].y, balls[i].z);
+
+    // console.log(sphere);
 
 };
 
-function makeCylinders(i) {
+function makeConnectors(i) {
 
-    const geometry = new THREE.CylinderGeometry(cylinderPositions[i].r, cylinderPositions[i].r, cylinderPositions[i].l, 64);
+    const geometry = new THREE.CylinderGeometry(connectors[i].r, connectors[i].r, connectors[i].l, 64);
     const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
     cylinder = new THREE.Mesh( geometry, material );
     container.add(cylinder);
@@ -273,34 +382,99 @@ function makeCylinders(i) {
 
     cylinder.rotation.x = Math.PI / 2;
 
-    if (cylinderPositions[i].d) {
-        cylinder.rotation.z = cylinderPositions[i].d;
+    if (connectors[i].d) {
+        cylinder.rotation.z = connectors[i].d;
     };
 
-    cylinder.position.set(cylinderPositions[i].x, cylinderPositions[i].y, cylinderPositions[i].z);
+    cylinder.position.set(connectors[i].x, connectors[i].y, connectors[i].z);
 
 };
 
-function onMouseMove( e ) {
+function makeLegs(i) {
+
+    const geometry = new THREE.CylinderGeometry(legs[i].r, legs[i].r, legs[i].l, 64);
+    const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+    leg = new THREE.Mesh( geometry, material );
+    container.add(leg);
+
+    leg.highlight = false;
+
+    // leg.rotation.x = Math.PI / 2;
+
+    // if (legs[i].d) {
+    //     leg.rotation.z = legs[i].d;
+    // };
+
+    leg.position.set(legs[i].x, legs[i].y, legs[i].z);
+
+};
+
+function onMouseMove(e) {
 
 	// calculate mouse position in normalized device coordinates
 	// (-1 to +1) for both components
 
-	mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+	mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+	mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+
+};
+
+function onClick() {
+
+    const intersects = raycaster.intersectObjects(container.children);
+    for (var i = 0; i < intersects.length; i++) {
+        if (intersects[i].object.highlight) {
+            // console.log(intersects[i].object.position);
+            let pos = intersects[i].object.position;
+            moveCamera(intersects[i].object.position);
+            // camera.position.set(pos.x + 5, pos.y + 5, pos.z + 5);
+            // camera.lookAt(pos);
+        };
+    };
+
+};
+
+function moveCamera(pos) {
+
+    // save original rotation and position
+    var startRotation = new THREE.Euler().copy(camera.rotation);
+    var startPosition = new THREE.Vector3().copy(camera.position);
+    // console.log('start', camera.rotation);
+
+    // final rotation (with lookAt)
+    camera.position.set(pos.x + 5, pos.y + 5, pos.z + 5);
+    camera.lookAt(pos);
+    var endRotation = new THREE.Euler().copy(camera.rotation);
+
+    // revert to original rotation and position
+    camera.rotation.copy(startRotation);
+    camera.position.copy(startPosition);
+
+    var tweenMove = new TWEEN.Tween(camera.position)
+    .to({x: pos.x + 5, y: pos.y + 5, z: pos.z + 5}, 1000)
+    .easing(TWEEN.Easing.Quartic.InOut)
+    .start();
+
+    var tweenRotate = new TWEEN.Tween(camera.rotation)
+    .to({x: endRotation.x, y: endRotation.y, z: endRotation.z}, 1000)
+    .easing(TWEEN.Easing.Quartic.InOut)
+    .start();
 
 };
 
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
+
+    TWEEN.update();
 
     // update the picking ray with the camera and mouse position
-	raycaster.setFromCamera( mouse, camera );
+	raycaster.setFromCamera(mouse, camera);
 
 	// calculate objects intersecting the picking ray
 	const intersects = raycaster.intersectObjects(container.children);
 
     for (var i = 0; i < container.children.length; i++) {
+        // update(container.children[i]);
         if (container.children[i].highlight) {
             container.children[i].material.color.set(0x00ff00);
         };
@@ -320,13 +494,14 @@ function animate() {
     };
 
     if (container) {
-        container.rotation.x += 0.002;
+        // container.rotation.x += 0.002;
     };
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 };
 
-window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('click', onClick, false);
 
 init();
 animate();
